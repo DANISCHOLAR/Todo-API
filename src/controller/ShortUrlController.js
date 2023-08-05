7cbde6d774514d1dfb4ee91a751860b156ec83f4
Danischolar <olayinkaoluwaseun90@gmail.com>
2023-08-06 00:30:55 +0100

    worked on controller dir

---
+++ src/controller/ShortUrlController.js
@@ -0,0 +1,13 @@
+const ShortUrl = require ("../models/ShortUrl");
+
+const redirect = async (req, res) => {
+
+
+}
+
+const store = async (req, res) => {
+
+
+}
+
+module.exports = { redirect, store}
