adee8085fb5def335d4203865b30b67e96976a1e
Danischolar <olayinkaoluwaseun90@gmail.com>
2023-08-06 00:22:09 +0100

    models

--- src/models/ShortUrl.js
+++ src/models/ShortUrl.js
@@ -2,6 +2,14 @@ const mongoose = require('mongoose')
 
 const ShortUrl = mongoose.Schema({
     alias: {
-        type: 
+        type: String,
+        unique: true,
+        rquired: true,
+    },
+    url: {
+        type: String,
+        required: true,
     }
 })
+
+module.exports =mongoose.model('ShortUrl', ShortUrl)
