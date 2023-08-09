e8696479b69e7c96a03701027fb29e24922b0d93
Danischolar <olayinkaoluwaseun90@gmail.com>
2023-08-09 02:10:38 +0100

    Controllers and Routes [2:10am]

--- src/controller/ShortUrlController.js
+++ src/controller/ShortUrlController.js
@@ -1,13 +1,51 @@
-const ShortUrl = require ("../models/ShortUrl");
+const ShortUrl = require("../models/ShortUrl");
+
+const { nanoid } = require("nanoid");
+const yup = require("yup");
+const newUrlSchema = yup.object().shape({
+  slug: yup
+    .string()
+    .trim()
+    .matches(/^[\w\-]+$/i),
+  url: yup.string().trim().url().required(),
+});
 
 const redirect = async (req, res) => {
+    const { id:alias} = req.params
 
-
-}
+    try{
+        const url = await ShortUrl.findOne({alias})
+        if (url) {
+            return res.redirect(url.url)
+        } else{ 
+            return res.status(404).send({message: "invalid url "})
+        }
+    } catch (error) {
+        return res.status(500).send({message:'invalid url'})
+    }
+};
 
 const store = async (req, res) => {
+  let { alias, url } = req.body;
 
+  try {
+    await newUrlSchema.validate({ alias, url });
 
-}
+    if (!alias) {
+      alias = nanoid(5);
+    } else {
+      const existing = await ShortUrl.findOne({ alias });
+      if (existing) {
+        throw new Error("Alias already in use");
+      }
+    }
+    alias = alias.toLowerCase();
+    const newShortUrl = { alias, url };
+    const created = await ShortUrl.create(newShortUrl);
+    res.json(created);
+  } catch (error) {
+    next(error);
+  }
+};
 
-module.exports = { redirect, store}
+module.exports = { redirect, store };
--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,10 +1,15 @@
 const router = require("express").Router();
 
+
+const ShortUrlController = require("../controller/ShortUrlController")
+
 //sets up routes
 
 router.get("/", (req, res) => {
   res.send("Shortening URLs for ya");
 });
+
+
 
 router.get("/:id", (req, res) => {
 
